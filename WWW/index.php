1c706b2c8d0b83edc336e9cd2e1a29169476d4b6
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-17 23:41:01 +0900

    rest api in php with curl

--- WWW/index.php
+++ WWW/index.php
@@ -1,34 +1,20 @@
 <?php
 
-// procedure
 $curl_init = curl_init();
-// curl_setopt($curl_init, CURLOPT_URL, "https://randomuser.me/api");
-// curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, true);
 
-$headers = [
-    "Authorization: token YOUR_ACCESS_KEY",
-];
-
-$payload = json_encode([
-    "name" => "Created fro API",
-    "description" => "an example API-created repo"
-]);
-
-// can be handled as an array.
 curl_setopt_array($curl_init, [
-    CURLOPT_URL => "https://api.github.com/orgs/satoru-oka/repos",
+    CURLOPT_URL => "https://api.github.com/gists",
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => $headers,
     CURLOPT_USERAGENT => "satoru-oka",
-    CURLOPT_POST => "POST",
-    CURLOPT_POSTFIELDS => $payload
 ]);
 
 $response = curl_exec($curl_init);
-$status_code = curl_getinfo($curl_init, CURLINFO_HTTP_CODE);
 
 curl_close($curl_init);
 
-echo $status_code, "\n";
+$data = json_decode($response, true);
+// print_r($data);
 
-echo $response, "\n";
+foreach ($data as $gist) {
+    echo $gist["id"]. " - " . $gist["description"];
+}
