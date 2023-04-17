97d220cd734c716973e263fecde777b59f66fd9b
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-17 17:51:17 +0900

    specifc request header

--- WWW/index.php
+++ WWW/index.php
@@ -6,30 +6,15 @@ $curl_init = curl_init();
 // curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, true);
 
 $headers = [
-    "Authorization: Client - ID YOUR_ACCESS_KEY"
+    "Authorization: token YOUR_ACCESS_KEY",
 ];
-
-$response_headers = [];
-
-$header_callback = function($curl_init, $header) use (&$response_headers) {
-    $len = strlen($header);
-    $parts = explode(":", $header, 2);
-
-    if (count($parts) < 2) {
-        return $len;
-    }
-
-    $response_headers[$parts[0]] = trim($parts[1]);
-
-    return $len;
-};
 
 // can be handled as an array.
 curl_setopt_array($curl_init, [
-    CURLOPT_URL => "https://example.com",
+    CURLOPT_URL => "https://api.github.com/repos/satoru-oka/api/stargazers",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER => $headers,
-    CURLOPT_HEADERFUNCTION => $header_callback
+    CURLOPT_USERAGENT => "satoru-oka"
 ]);
 
 $response = curl_exec($curl_init);
@@ -38,7 +23,5 @@ $status_code = curl_getinfo($curl_init, CURLINFO_HTTP_CODE);
 curl_close($curl_init);
 
 echo $status_code, "\n";
-
-print_r($response_headers);
 
 echo $response, "\n";
