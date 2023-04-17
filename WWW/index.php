96d911867653bd3c14c4d5ff7789353348ef3fc8
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-17 15:56:09 +0900

    response code

--- WWW/index.php
+++ WWW/index.php
@@ -7,11 +7,15 @@ $curl_init = curl_init();
 
 // can be handled as an array.
 curl_setopt_array($curl_init, [
-    CURLOPT_URL => "https://randomuser.me/api",
+    CURLOPT_URL => "https://maps.openweathermap.org/maps/2.0/weather/1h/HRD0/4/1/6?date={API}",
     CURLOPT_RETURNTRANSFER => true
 ]);
 
 $response = curl_exec($curl_init);
+$status_code = curl_getinfo($curl_init, CURLINFO_HTTP_CODE);
+
 curl_close($curl_init);
+
+echo $status_code;
 
 echo $response, "\n";
