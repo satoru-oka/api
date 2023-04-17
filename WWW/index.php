3ade54e53173254616b56bd2b1faa85b41743248
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-17 15:13:51 +0900

    use cURL

--- WWW/index.php
+++ WWW/index.php
@@ -1,45 +1,17 @@
 <?php
 
-// Entering a name returns the age.
-if (!empty($_GET["name"])) {
-    $response = file_get_contents("https://api.agify.io?name={$_GET['name']}");
-    $data = json_decode($response, true);
-    $age = $data["age"];
-}
+// procedure
+$curl_init = curl_init();
+// curl_setopt($curl_init, CURLOPT_URL, "https://randomuser.me/api");
+// curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, true);
 
-// $response = file_get_contents("https://example.com");
-// $response = file_get_contents("https://randomuser.me/api");
+// can be handled as an array.
+curl_setopt_array($curl_init, [
+    CURLOPT_URL => "https://randomuser.me/api",
+    CURLOPT_RETURNTRANSFER => true
+]);
 
-// $data = json_decode($response, true);
+$response = curl_exec($curl_init);
+curl_close($curl_init);
 
-// accessing arrays
-// echo $data["results"][0]["name"]["first"], "\n";
-// array
-// var_dump($data);
-// json
-// echo $response;
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Example</title>
-</head>
-<body>
-
-    <?php if (isset($age)): ?>
-        Age: <?= $age ?>
-    <?php endif; ?>
-    
-    <form action="">
-        <label for="name">Name</label>
-        <input name="name" id="name">
-
-        <button>Guess age</button>
-    </form>
-
-</body>
-</html>
+echo $response, "\n";
