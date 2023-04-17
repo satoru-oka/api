af8fc929b53ec99cb0820457d1e87e114904ee4e
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-17 17:03:04 +0900

    response header

--- WWW/index.php
+++ WWW/index.php
@@ -12,14 +12,19 @@ $headers = [
 curl_setopt_array($curl_init, [
     CURLOPT_URL => "https://example.com",
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => $headers
+    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_HEADER => ture
 ]);
 
 $response = curl_exec($curl_init);
 $status_code = curl_getinfo($curl_init, CURLINFO_HTTP_CODE);
+$status_type = curl_getinfo($curl_init, CURLINFO_CONTENT_TYPE);
+$status_length = curl_getinfo($curl_init, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
 
 curl_close($curl_init);
 
-echo $status_code;
+echo $status_code, "\n";
+echo $status_type, "\n";
+echo $status_length, "\n";
 
 echo $response, "\n";
