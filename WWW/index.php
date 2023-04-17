615f7ce314286da9fa7b4a00599e785c66be8c27
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-17 14:40:35 +0900

    api-result

--- WWW/index.php
+++ WWW/index.php
@@ -1,10 +1,13 @@
 <?php
-// api call
-$response = file_get_contents("https://example.com");
-// $response = file_get_contents("https://randomuser.me/api");
 
-// $data = json_decode($response, true);
+// $response = file_get_contents("https://example.com");
+$response = file_get_contents("https://randomuser.me/api");
 
-// echo $data["results"][0]["name"]["first"], "\n";
-// var_dump($data);
+$data = json_decode($response, true);
+
+// accessing arrays
+echo $data["results"][0]["name"]["first"], "\n";
+// array
+var_dump($data);
+// json
 echo $response;
