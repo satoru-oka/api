754a4c8f8e0f80b84f6c182c713eb8b07e13d357
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-17 17:20:51 +0900

    get all individual

--- WWW/index.php
+++ WWW/index.php
@@ -4,27 +4,41 @@
 $curl_init = curl_init();
 // curl_setopt($curl_init, CURLOPT_URL, "https://randomuser.me/api");
 // curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, true);
+
 $headers = [
-    "Authorization: Client - ID "
+    "Authorization: Client - ID YOUR_ACCESS_KEY"
 ];
+
+$response_headers = [];
+
+$header_callback = function($curl_init, $header) use (&$response_headers) {
+    $len = strlen($header);
+    $parts = explode(":", $header, 2);
+
+    if (count($parts) < 2) {
+        return $len;
+    }
+
+    $response_headers[$parts[0]] = trim($parts[1]);
+
+    return $len;
+};
 
 // can be handled as an array.
 curl_setopt_array($curl_init, [
     CURLOPT_URL => "https://example.com",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER => $headers,
-    CURLOPT_HEADER => ture
+    CURLOPT_HEADERFUNCTION => $header_callback
 ]);
 
 $response = curl_exec($curl_init);
 $status_code = curl_getinfo($curl_init, CURLINFO_HTTP_CODE);
-$status_type = curl_getinfo($curl_init, CURLINFO_CONTENT_TYPE);
-$status_length = curl_getinfo($curl_init, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
 
 curl_close($curl_init);
 
 echo $status_code, "\n";
-echo $status_type, "\n";
-echo $status_length, "\n";
+
+print_r($response_headers);
 
 echo $response, "\n";
