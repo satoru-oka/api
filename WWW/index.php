03ec19a51f8d6c29bec6eaac59fcff03c7b4f53b
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-17 16:56:47 +0900

    request header sample

--- WWW/index.php
+++ WWW/index.php
@@ -4,11 +4,15 @@
 $curl_init = curl_init();
 // curl_setopt($curl_init, CURLOPT_URL, "https://randomuser.me/api");
 // curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, true);
+$headers = [
+    "Authorization: Client - ID "
+];
 
 // can be handled as an array.
 curl_setopt_array($curl_init, [
-    CURLOPT_URL => "https://maps.openweathermap.org/maps/2.0/weather/1h/HRD0/4/1/6?date={API}",
-    CURLOPT_RETURNTRANSFER => true
+    CURLOPT_URL => "https://example.com",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => $headers
 ]);
 
 $response = curl_exec($curl_init);
