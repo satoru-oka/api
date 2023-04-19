d07cde0600fc2530638f8c5a51877e98f1c88fd8
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-19 11:39:16 +0900

    get the resource, id and the request method

--- WWW/api/index.php
+++ WWW/api/index.php
@@ -1,2 +1,8 @@
 <?php
-echo "hello world";
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$parts = explode("/", $path);
+$resource = $parts[4];
+$id = $parts[5] ?? null;
+
+echo  $resource . ", " . $id;
+echo $_SERVER["REQUEST_METHOD"];
