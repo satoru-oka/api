966f9c2be30a5edfe41e1b934cfea2da093874f7
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-19 15:00:31 +0900

    make debugging easier

--- WWW/api/index.php
+++ WWW/api/index.php
@@ -1,6 +1,9 @@
 <?php
 // fatal error:Your Composer dependencies require a PHP version ">= 8.1.0". You are running 8.0.8.
 // require dirname(__DIR__) . "/vendor/autoload.php";
+declare(strict_types=1);
+ini_set("display_erros", "On");
+
 require dirname(__DIR__) . "/src/TaskController.php";
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
