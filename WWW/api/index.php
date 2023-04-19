96172c1362a9fdca64072abc574782767adcc47d
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-19 14:53:15 +0900

    add controller class to decide the repoonse

--- WWW/api/index.php
+++ WWW/api/index.php
@@ -1,4 +1,8 @@
 <?php
+// fatal error:Your Composer dependencies require a PHP version ">= 8.1.0". You are running 8.0.8.
+// require dirname(__DIR__) . "/vendor/autoload.php";
+require dirname(__DIR__) . "/src/TaskController.php";
+
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $parts = explode("/", $path);
 $resource = $parts[4];
@@ -9,7 +13,6 @@ if ($resource != "tasks") {
     exit;
 }
 
-require dirname(__DIR__) . "/src/TaskController.php";
 
 $controller = new TaskController;
 $controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
